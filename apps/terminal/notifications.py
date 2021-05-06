3287637434cd4e1c44cacc1eead20c88490a9127
xinwen <coderWen@126.com>
2021-05-06 10:01:57 +0800

    添加命令告警

--- apps/terminal/notifications.py
+++ apps/terminal/notifications.py
@@ -8,22 +8,37 @@ logger = get_logger(__name__)
 
 
 class CommandAlertMessage(MessageBase):
-    message_label = 'Command Alert'
+    message_label = _('Command Alert')
 
-    def get_common_msg(self, command):
+    def _get_message(self, command):
         session_obj = Session.objects.get(id=command['session'])
 
-        input = command['input']
-        if isinstance(input, str):
-            input = input.replace('\r\n', ' ').replace('\r', ' ').replace('\n', ' ')
-
-        msg = _("Insecure Command Alert: [%(name)s->%(login_from)s@%(remote_addr)s] $%(command)s") % {
-            'name': command['user'],
-            'login_from': session_obj.get_login_from_display(),
-            'remote_addr': session_obj.remote_addr,
-            'command': input
+        message = _("""
+                        Command: %(command)s
+                        <br>
+                        Asset: %(host_name)s (%(host_ip)s)
+                        <br>
+                        User: %(user)s
+                        <br>
+                        Level: %(risk_level)s
+                        <br>
+                        Session: <a href="%(session_detail_url)s">session detail</a>
+                        <br>
+                        """) % {
+            'command': command['input'],
+            'host_name': command['asset'],
+            'host_ip': session_obj.asset_obj.ip,
+            'user': command['user'],
+            'risk_level': Command.get_risk_level_str(command['risk_level']),
+            'session_detail_url': reverse('api-terminal:session-detail',
+                                          kwargs={'pk': command['session']},
+                                          external=True, api_to_ui=True),
         }
-        return msg
+
+        return message
+
+    def get_common_msg(self, command):
+        return self._get_message(command)
 
     def get_email_msg(self, command):
         session_obj = Session.objects.get(id=command['session'])
@@ -39,27 +54,50 @@ class CommandAlertMessage(MessageBase):
             'command': input
         }
 
+        message = self._get_message(command)
+
+        return {
+            'subject': subject,
+            'message': message
+        }
+
+
+class CommandExecutionAlert(MessageBase):
+    message_label = _('Batch command alert')
+
+    def _get_message(self, command):
+        input = command['input']
+        input = input.replace('\n', '<br>')
+
+        assets = ', '.join([str(asset) for asset in command['assets']])
         message = _("""
-                Command: %(command)s
-                <br>
-                Asset: %(host_name)s (%(host_ip)s)
-                <br>
-                User: %(user)s
-                <br>
-                Level: %(risk_level)s
-                <br>
-                Session: <a href="%(session_detail_url)s">session detail</a>
-                <br>
-                """) % {
-            'command': command['input'],
-            'host_name': command['asset'],
-            'host_ip': session_obj.asset_obj.ip,
+                            <br>
+                            Assets: %(assets)s
+                            <br>
+                            User: %(user)s
+                            <br>
+                            Level: %(risk_level)s
+                            <br>
+
+                            ----------------- Commands ---------------- <br>
+                            %(command)s <br>
+                            ----------------- Commands ---------------- <br>
+                            """) % {
+            'command': input,
+            'assets': assets,
             'user': command['user'],
             'risk_level': Command.get_risk_level_str(command['risk_level']),
-            'session_detail_url': reverse('api-terminal:session-detail',
-                                          kwargs={'pk': command['session']},
-                                          external=True, api_to_ui=True),
         }
+        return message
+
+    def get_common_msg(self, command):
+        return self._get_message(command)
+
+    def get_email_msg(self, command):
+        subject = _("Insecure Web Command Execution Alert: [%(name)s]") % {
+            'name': command['user'],
+        }
+        message = self._get_message(command)
 
         return {
             'subject': subject,
